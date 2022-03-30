7b76ab0c2ccbb7b2decee493c690a95c0ec9b094
João Araújo <joaogabriel-al@hotmail.com>
2022-03-30 07:52:30 -0300

    03 - 02.Estilizando Componentes

--- src/components/ListaDeNotas.jsx
+++ src/components/ListaDeNotas.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import CardNota from "./CardNota";
+import CardNota from "./CardNota/CardNota";
 class ListaDeNotas extends Component {
   render() {
     return (
       <ul>
-        {/* Método Array.of() cria um array */}
-        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria) => {
+        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria, index) => {
           return (
-            <li>
+            // key é um identificador necessário para o React saber exatamente qual elemento
+            // ele deve re-redenrizar
+            <li key={index}>
               <div>{categoria}</div>
               <CardNota />
             </li>
