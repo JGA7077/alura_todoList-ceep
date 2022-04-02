e6f347f2184c63d25f743b60a0ca98e15b311dc3
João Araújo <joaogabriel-al@hotmail.com>
2022-04-02 11:02:00 -0300

    05 03.Mais propriedades

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/FormularioCadastro";
 import "./assets/App.css";
 class App extends Component {
+  constructor() {
+    super();
+    this.notas = [];
+  }
 
   criarNota(titulo, texto) {
-
-    console.log(`recebeu as informações ${titulo} ${texto}`);
+    const notaNota = { titulo, texto };
+    this.notas.push(notaNota)
   }
 
   render() {
     return (
       <section className="conteudo">
-        <FormularioCadastro criarNota={this.criarNota} />
-        <ListaDeNotas />
+        <FormularioCadastro criarNota={this.criarNota.bind(this)} />
+        <ListaDeNotas notas={this.notas} />
       </section>
     );
   }
