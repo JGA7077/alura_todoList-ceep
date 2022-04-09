0abd6027afe45d926573757ce0844102ceb8577e
João Araújo <joaogabriel-al@hotmail.com>
2022-04-09 14:44:29 -0300

    02 - 03.Statefull e stateless Componentes

--- src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,19 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./estilo.css";
 
-class ListaDeCategorias extends Component{
+class ListaDeCategorias extends Component {
+
+  _handleEventoInput(e) {
+    console.log('key', e.key)
+    if(e.key == "Enter") {
+      console.log(e.target.value)
+    }
+  }
   render() {
-    return(
-      <section>
-        <ul>
-          <li>Categorias</li>
-          <li>Categorias</li>
-          <li>Categorias</li>
-          <li>Categorias</li>
-          <li>Categorias</li>
+    return (
+      <section className="lista-categorias">
+        <ul className="lista-categorias_lista">
+          <li className="lista-categorias_item">Categorias</li>
+          <li className="lista-categorias_item">Categorias</li>
+          <li className="lista-categorias_item">Categorias</li>
+          <li className="lista-categorias_item">Categorias</li>
+          <li className="lista-categorias_item">Categorias</li>
         </ul>
-        <input type="text" />
+        <input 
+          type="text"
+          className="lista-categorias_input"
+          placeholder="Adicionar Categoria" 
+          onKeyUp={this._handleEventoInput.bind(this)}
+        />
       </section>
-    )
+    );
   }
 }
 
